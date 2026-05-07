7c83bfca8ff1a643ba07a80a5d34088f4f862290
yorshkasencho <56592074+yorshkasencho@users.noreply.github.com>
2026-05-07 11:54:13 +0900

    Update behavior_battery_led.c

--- src/behaviors/behavior_battery_led.c
+++ src/behaviors/behavior_battery_led.c
@@ -1,113 +1,105 @@
 #include <zephyr/device.h>
-#include <zephyr/devicetree.h>
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 
-#include <zmk/battery.h>
 #include <zmk/behavior.h>
-#include <zmk/behavior_queue.h>
 #include <zmk/keymap.h>
 
 #define DT_DRV_COMPAT zmk_behavior_battery_led
 
 /*
- * XIAO nRF52840 onboard RGB USER LED
+ * XIAO nRF52840 onboard RGB LED direct GPIO test
  *
- * These LEDs are active-low:
- *   gpio_pin_set_dt(..., 0) = ON
- *   gpio_pin_set_dt(..., 1) = OFF
+ * RGB LED pins:
+ * Red   = P0.26
+ * Green = P0.30
+ * Blue  = P0.06
+ *
+ * XIAO onboard RGB LED is active-low:
+ * 0 = ON
+ * 1 = OFF
  */
-static const struct gpio_dt_spec led_red = GPIO_DT_SPEC_GET(DT_NODELABEL(led0), gpios);
-static const struct gpio_dt_spec led_green = GPIO_DT_SPEC_GET(DT_NODELABEL(led1), gpios);
-static const struct gpio_dt_spec led_blue = GPIO_DT_SPEC_GET(DT_NODELABEL(led2), gpios);
 
-static struct k_work_delayable battery_led_work;
-static int blink_count;
-static bool blink_on;
+#define LED_PORT_NODE DT_NODELABEL(gpio0)
 
-enum battery_led_color {
-    BATTERY_LED_RED,
-    BATTERY_LED_GREEN,
-    BATTERY_LED_BLUE,
-    BATTERY_LED_YELLOW,
-};
+static const struct device *gpio0_dev = DEVICE_DT_GET(LED_PORT_NODE);
 
-static enum battery_led_color current_color = BATTERY_LED_RED;
+static struct k_work_delayable led_work;
+static int step = 0;
 
-static void battery_led_all_off(void) {
-    gpio_pin_set_dt(&led_red, 1);
-    gpio_pin_set_dt(&led_green, 1);
-    gpio_pin_set_dt(&led_blue, 1);
+static void all_off(void) {
+    gpio_pin_set_raw(gpio0_dev, 26, 1); /* Red off */
+    gpio_pin_set_raw(gpio0_dev, 30, 1); /* Green off */
+    gpio_pin_set_raw(gpio0_dev, 6, 1);  /* Blue off */
 }
 
-static void battery_led_show_color(enum battery_led_color color) {
-    battery_led_all_off();
+static void show_red(void) {
+    all_off();
+    gpio_pin_set_raw(gpio0_dev, 26, 0);
+}
 
-    switch (color) {
-    case BATTERY_LED_BLUE:
-        gpio_pin_set_dt(&led_blue, 0);
+static void show_green(void) {
+    all_off();
+    gpio_pin_set_raw(gpio0_dev, 30, 0);
+}
+
+static void show_blue(void) {
+    all_off();
+    gpio_pin_set_raw(gpio0_dev, 6, 0);
+}
+
+static void show_yellow(void) {
+    all_off();
+    gpio_pin_set_raw(gpio0_dev, 26, 0);
+    gpio_pin_set_raw(gpio0_dev, 30, 0);
+}
+
+static void led_work_handler(struct k_work *work) {
+    switch (step) {
+    case 0:
+        show_red();
         break;
-    case BATTERY_LED_GREEN:
-        gpio_pin_set_dt(&led_green, 0);
+    case 1:
+        all_off();
         break;
-    case BATTERY_LED_YELLOW:
-        gpio_pin_set_dt(&led_red, 0);
-        gpio_pin_set_dt(&led_green, 0);
+    case 2:
+        show_green();
         break;
-    case BATTERY_LED_RED:
+    case 3:
+        all_off();
+        break;
+    case 4:
+        show_blue();
+        break;
+    case 5:
+        all_off();
+        break;
+    case 6:
+        show_yellow();
+        break;
+    case 7:
+        all_off();
+        break;
     default:
-        gpio_pin_set_dt(&led_red, 0);
-        break;
-    }
-}
-
-static enum battery_led_color battery_led_color_from_percent(uint8_t percent) {
-    if (percent >= 90) {
-        return BATTERY_LED_BLUE;
-    } else if (percent >= 50) {
-        return BATTERY_LED_GREEN;
-    } else if (percent >= 20) {
-        return BATTERY_LED_YELLOW;
-    } else {
-        return BATTERY_LED_RED;
-    }
-}
-
-static void battery_led_blink_work_handler(struct k_work *work) {
-    if (blink_count <= 0) {
-        battery_led_all_off();
+        all_off();
         return;
     }
 
-    if (blink_on) {
-        battery_led_all_off();
-    } else {
-        battery_led_show_color(current_color);
-    }
-
-    blink_on = !blink_on;
-    blink_count--;
-
-    k_work_schedule(&battery_led_work, K_MSEC(250));
+    step++;
+    k_work_schedule(&led_work, K_MSEC(300));
 }
 
-static int battery_led_start_blink(void) {
-    uint8_t percent = zmk_battery_state_of_charge();
-
-    current_color = battery_led_color_from_percent(percent);
-
-    blink_count = 12; /* 250ms x 12 = about 3 seconds */
-    blink_on = false;
-
-    k_work_cancel_delayable(&battery_led_work);
-    k_work_schedule(&battery_led_work, K_NO_WAIT);
+static int battery_led_start_test(void) {
+    step = 0;
+    k_work_cancel_delayable(&led_work);
+    k_work_schedule(&led_work, K_NO_WAIT);
 
     return ZMK_BEHAVIOR_OPAQUE;
 }
 
 static int on_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                      struct zmk_behavior_binding_event event) {
-    return battery_led_start_blink();
+    return battery_led_start_test();
 }
 
 static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
@@ -116,19 +108,17 @@ static int on_keymap_binding_released(struct zmk_behavior_binding *binding,
 }
 
 static int behavior_battery_led_init(const struct device *dev) {
-    if (!device_is_ready(led_red.port) ||
-        !device_is_ready(led_green.port) ||
-        !device_is_ready(led_blue.port)) {
+    if (!device_is_ready(gpio0_dev)) {
         return -ENODEV;
     }
 
-    gpio_pin_configure_dt(&led_red, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&led_green, GPIO_OUTPUT_INACTIVE);
-    gpio_pin_configure_dt(&led_blue, GPIO_OUTPUT_INACTIVE);
+    gpio_pin_configure(gpio0_dev, 26, GPIO_OUTPUT_HIGH);
+    gpio_pin_configure(gpio0_dev, 30, GPIO_OUTPUT_HIGH);
+    gpio_pin_configure(gpio0_dev, 6, GPIO_OUTPUT_HIGH);
 
-    battery_led_all_off();
+    all_off();
 
-    k_work_init_delayable(&battery_led_work, battery_led_blink_work_handler);
+    k_work_init_delayable(&led_work, led_work_handler);
 
     return 0;
 }
